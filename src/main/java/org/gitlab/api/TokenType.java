f328e64f53c4488ea45c046f38a6332cf70089e8
Tim Olshansky <tim.olshansky@gmail.com>
2015-11-04 10:13:25 -0800

    Resolve whitespace issues

--- src/main/java/org/gitlab/api/TokenType.java
+++ src/main/java/org/gitlab/api/TokenType.java
@@ -1,35 +1,35 @@
 package org.gitlab.api;
 
 public enum TokenType {
-	PRIVATE_TOKEN("private_token", "PRIVATE-TOKEN", "%s"),
-	ACCESS_TOKEN("access_token", "Authorization", "Bearer %s");
+    PRIVATE_TOKEN("private_token", "PRIVATE-TOKEN", "%s"),
+    ACCESS_TOKEN("access_token", "Authorization", "Bearer %s");
 
-	private final String tokenParamName;
-	private final String tokenHeaderName;
-	private final String tokenHeaderFormat;
+    private final String tokenParamName;
+    private final String tokenHeaderName;
+    private final String tokenHeaderFormat;
 
     /**
      * Constructor
      *
      * @param tokenParamName    The url parameter name when using AuthMethod.URL_PARAMETER
-	 * @param tokenHeaderName   The header name when using AuthMethod.HEADER
-	 * @param tokenHeaderFormat The header format for String.format when using AuthMethod.HEADER
+     * @param tokenHeaderName   The header name when using AuthMethod.HEADER
+     * @param tokenHeaderFormat The header format for String.format when using AuthMethod.HEADER
      */
-	TokenType(String tokenParamName, String tokenHeaderName, String tokenHeaderFormat) {
-		this.tokenParamName = tokenParamName;
-		this.tokenHeaderName = tokenHeaderName;
-		this.tokenHeaderFormat = tokenHeaderFormat;
-	}
+    TokenType(String tokenParamName, String tokenHeaderName, String tokenHeaderFormat) {
+        this.tokenParamName = tokenParamName;
+        this.tokenHeaderName = tokenHeaderName;
+        this.tokenHeaderFormat = tokenHeaderFormat;
+    }
 
-	public String getTokenParamName() {
-		return tokenParamName;
-	}
+    public String getTokenParamName() {
+        return tokenParamName;
+    }
 
-	public String getTokenHeaderName() {
-		return tokenHeaderName;
-	}
+    public String getTokenHeaderName() {
+        return tokenHeaderName;
+    }
 
-	public String getTokenHeaderFormat() {
-		return tokenHeaderFormat;
-	}
+    public String getTokenHeaderFormat() {
+        return tokenHeaderFormat;
+    }
 }
