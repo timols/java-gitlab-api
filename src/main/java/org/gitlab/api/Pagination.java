0caa454420cf569222a4e1761253bc36a5121a23
caguilar187 <caguilar187@gmail.com>
2016-09-06 10:09:30 -0400

    changing implement to extend since Query is now Abstract
    removing un-needed fields and try catches since it now extends query

--- src/main/java/org/gitlab/api/Pagination.java
+++ src/main/java/org/gitlab/api/Pagination.java
@@ -5,35 +5,19 @@ import org.gitlab.api.queries.QueryBuilder;
 
 import java.io.UnsupportedEncodingException;
 
-public class Pagination implements Query {
+public class Pagination extends Query {
     public static final String PARAM_PAGE = "page";
     public static final String PARAM_PER_PAGE = "per_page";
     public static final int MAX_ITEMS_PER_PAGE = 100;
-    private final QueryBuilder queryBuilder = new QueryBuilder();
 
     public void setPage(int page) {
-        try {
-            queryBuilder.append(PARAM_PAGE, String.valueOf(page));
-        } catch (UnsupportedEncodingException ignored) {
-        }
+        doAppend(PARAM_PAGE, String.valueOf(page));
     }
 
     public void setPerPage(int perPage) {
         if (perPage > MAX_ITEMS_PER_PAGE) {
             throw new IllegalArgumentException("Max value for perPage is " + MAX_ITEMS_PER_PAGE);
         }
-        try {
-            queryBuilder.append(PARAM_PER_PAGE, String.valueOf(perPage));
-        } catch (UnsupportedEncodingException ignored) {
-        }
-    }
-
-    public QueryBuilder asBuilder() {
-        return queryBuilder;
-    }
-
-    @Override
-    public String toString() {
-        return queryBuilder.toString();
+        doAppend(PARAM_PER_PAGE, String.valueOf(perPage));
     }
 }
