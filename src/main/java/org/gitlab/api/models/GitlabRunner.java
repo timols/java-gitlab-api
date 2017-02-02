567bc5b7be9b313b35da6f380e5f5e5f9c39fafe
Mirko Friedenhagen <mfriedenhagen@gmail.com>
2017-02-02 18:18:28 +0100

    Add additional fields for Runner.
    The current model is missing some fields.

--- src/main/java/org/gitlab/api/models/GitlabRunner.java
+++ src/main/java/org/gitlab/api/models/GitlabRunner.java
@@ -3,14 +3,27 @@ package org.gitlab.api.models;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 
-public class GitlabRunner {
+import java.util.Date;
+import java.util.List;
 
+public class GitlabRunner {
     private Integer id;
     private String description;
     private Boolean active;
     @JsonProperty("is_shared")
     private Boolean isShared;
     private String name;
+    private String version;
+    private String revision;
+    @JsonProperty("contacted_at")
+    private Date contactedAt;
+    @JsonProperty("tag_list")
+    private List<String> tagList;
+    @JsonProperty("run_untagged")
+    private Boolean runUntagged;
+    private Boolean locked;
+    private String platform;
+    private String architecture;
 
     public Integer getId() {
         return id;
@@ -51,4 +64,70 @@ public class GitlabRunner {
     public void setName(String name) {
         this.name = name;
     }
+
+    public String getVersion() {
+        return version;
+    }
+
+    public void setVersion(String version) {
+        this.version = version;
+    }
+
+    public String getRevision() {
+        return revision;
+    }
+
+    public void setRevision(String revision) {
+        this.revision = revision;
+    }
+
+    public Date getContactedAt() {
+        return contactedAt;
+    }
+
+    public void setContactedAt(Date contactedAt) {
+        this.contactedAt = contactedAt;
+    }
+
+    public List<String> getTagList() {
+        return tagList;
+    }
+
+    public void setTagList(List<String> tagList) {
+        this.tagList = tagList;
+    }
+
+    public Boolean isRunUntagged() {
+        return runUntagged;
+    }
+
+    public void setRunUntagged(boolean runUntagged) {
+        this.runUntagged = runUntagged;
+    }
+
+    public Boolean isLocked() {
+        return locked;
+    }
+
+    public void setLocked(boolean locked) {
+        this.locked = locked;
+    }
+
+
+    public String getPlatform() {
+        return platform;
+    }
+
+    public void setPlatform(String platform) {
+        this.platform = platform;
+    }
+
+    public String getArchitecture() {
+        return architecture;
+    }
+
+    public void setArchitecture(String architecture) {
+        this.architecture = architecture;
+    }
+
 }
