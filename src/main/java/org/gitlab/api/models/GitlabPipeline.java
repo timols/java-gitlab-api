57c979b9f90147745c94763558196fa9f98bf6a0
Florian Baader <flobaader@web.de>
2017-12-26 18:37:55 +0100

    Add pipeline field of GitlabCommit

--- src/main/java/org/gitlab/api/models/GitlabPipeline.java
+++ src/main/java/org/gitlab/api/models/GitlabPipeline.java
@@ -1,5 +1,52 @@
 package org.gitlab.api.models;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 public class GitlabPipeline {
     public static final String URL = "/pipelines";
+
+
+    @JsonProperty("id")
+    private Integer id;
+
+    @JsonProperty("ref")
+    private String ref;
+
+    @JsonProperty("sha")
+    private String sha;
+
+    @JsonProperty("status")
+    private String status;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getRef() {
+        return ref;
+    }
+
+    public void setRef(String ref) {
+        this.ref = ref;
+    }
+
+    public String getSha() {
+        return sha;
+    }
+
+    public void setSha(String sha) {
+        this.sha = sha;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
 }
