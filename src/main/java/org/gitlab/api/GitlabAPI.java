237770e90a4204431a4541bf8caf1a7ba6fee713
Bruno Ferreira <bmibferreira@gmail.com>
2018-02-27 13:40:42 +0000

    Added a new method to create a new SSH key
    Added a new method to create a new SSH key for the current user (https://docs.gitlab.com/ce/api/users.html#add-ssh-key) because the other method already available doesn't work if the user is not admin (https://docs.gitlab.com/ce/api/users.html#add-ssh-key-for-user) and wants to add a new SSH key for him.

--- src/main/java/org/gitlab/api/GitlabAPI.java
+++ src/main/java/org/gitlab/api/GitlabAPI.java
@@ -319,6 +319,26 @@ public class GitlabAPI {
 
         return dispatch().to(tailUrl, GitlabSSHKey.class);
     }
+    
+    /**
+     * Create a new ssh key for the authenticated user.
+     *
+     * @param targetUserId The id of the Gitlab user
+     * @param title        The title of the ssh key
+     * @param key          The public key
+     * @return The new GitlabSSHKey
+     * @throws IOException on gitlab api call error
+     */
+    public GitlabSSHKey createSSHKey(String title, String key) throws IOException {
+
+        Query query = new Query()
+                .append("title", title)
+                .append("key", key);
+
+        String tailUrl = GitlabUser.USERS_URL + "/" + GitlabSSHKey.KEYS_URL + query.toString();
+
+        return dispatch().to(tailUrl, GitlabSSHKey.class);
+    }
 
     /**
      * Delete user's ssh key
