8fa396dac3702b5bfce77917ed816abadb25a213
Bruno Ferreira <bmibferreira@gmail.com>
2018-02-27 13:51:47 +0000

    Removed extra "s" from the tailURL formation

--- src/main/java/org/gitlab/api/GitlabAPI.java
+++ src/main/java/org/gitlab/api/GitlabAPI.java
@@ -334,7 +334,7 @@ public class GitlabAPI {
                 .append("title", title)
                 .append("key", key);
 
-        String tailUrl = GitlabUser.USERS_URL + GitlabSSHKey.KEYS_URL + query.toString();
+        String tailUrl = GitlabUser.USER_URL + GitlabSSHKey.KEYS_URL + query.toString();
 
         return dispatch().to(tailUrl, GitlabSSHKey.class);
     }
