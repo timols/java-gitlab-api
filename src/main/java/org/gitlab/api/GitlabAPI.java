87a4a46a0215e479e7bc22ea4bd3384e5e800fbd
slauriere <slauriere@xwiki.com>
2019-08-13 20:40:31 +0200

    Files containing spaces in their path are wrongly encoded (#331) (#332)
    - Encode '+' characters in paths in '%20'

--- src/main/java/org/gitlab/api/GitlabAPI.java
+++ src/main/java/org/gitlab/api/GitlabAPI.java
@@ -3290,7 +3290,7 @@ public class GitlabAPI {
 
     private String sanitizePath(String branch) {
         try {
-            return URLEncoder.encode(branch, "UTF-8");
+            return URLEncoder.encode(branch, "UTF-8").replaceAll("\\+", "%20");
         } catch (UnsupportedEncodingException e) {
             throw new RuntimeException((e));
         }
